8dd3f7414df1083fa4af0c49d4751c02825a42bd
Nikita Smolianitski <n.smolianitski@gmail.com>
2023-11-13 11:01:00 +0300

    Fix closest enemy search in PistolAbilitySystem

--- Assets/Core/Scripts/Abilities/PistolAbilitySystem.cs
+++ Assets/Core/Scripts/Abilities/PistolAbilitySystem.cs
@@ -1,4 +1,5 @@
-﻿using BaboonAndCo.Extensions;
+﻿using System.Collections.Generic;
+using BaboonAndCo.Extensions;
 using BaboonAndCo.Utils;
 using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
@@ -15,7 +16,8 @@ namespace SpecialHedgehog.Abilities
 {
     public class PistolAbilitySystem : IEcsRunSystem
     {
-        private EcsFilterInject<Inc<PistolAbility, TransformRef>> _filter;
+        private EcsFilterInject<Inc<PistolAbility, TransformRef>> _pistolAbilityFilter;
+        private EcsFilterInject<Inc<Mob, TransformRef>> _mobFilter;
 
         private EcsPoolInject<ProjectileSpawnRequest> _projectileSpawnRequestPool = Constants.Worlds.Events;
         private EcsPoolInject<TransformRef> _transformRefPool;
@@ -23,34 +25,56 @@ namespace SpecialHedgehog.Abilities
         
         private EcsCustomInject<TimeService> _timeService;
         private EcsCustomInject<GameConfig> _gameConfig;
+        private EcsCustomInject<GameData> _gameData;
         private EcsCustomInject<AudioService> _audioService;
         
         public void Run(IEcsSystems systems)
         {
-            foreach (var entity in _filter.Value)
+            foreach (var entity in _pistolAbilityFilter.Value)
             {
                 ref var ability = ref _pistolAbilityPool.Value.Get(entity);
                 ability.Cooldown -= _timeService.Value.DeltaTime;
                 if (ability.Cooldown > 0) continue;
 
-                var mobs = Object.FindObjectsOfType<MobView>();
-                if (mobs.Length == 0)
-                    return;
-                
                 ref var transformRef = ref _transformRefPool.Value.Get(entity);
-                var closestEnemy = TransformUtils.GetClosestTargetToUnit(mobs, transformRef.Value);
+                var position = transformRef.Value.position;
+    
+                var closestEnemy = GetClosestTargetToUnit(_mobFilter.Value.GetRawEntities(), position);
+                if (closestEnemy is null)
+                    continue;
                 
                 ref var projectileSpawnRequest = ref _projectileSpawnRequestPool.NewEntity(out _);
                 projectileSpawnRequest.Damage = _gameConfig.Value.ProjectileDamage;
                 projectileSpawnRequest.Speed = _gameConfig.Value.ProjectileSpeed;
                 projectileSpawnRequest.Prefab = _gameConfig.Value.ProjectilePrefab;
-                projectileSpawnRequest.SpawnPoint = transformRef.Value.position;
-                projectileSpawnRequest.Direction = closestEnemy.transform.position - transformRef.Value.position;
+                projectileSpawnRequest.SpawnPoint = position;
+                projectileSpawnRequest.Direction = closestEnemy.transform.position - position;
                 
-                _audioService.Value.PlaySoundAtPoint(_gameConfig.Value.ShootSounds.GetRandom(), transformRef.Value.position);
+                _audioService.Value.PlaySoundAtPoint(_gameConfig.Value.ShootSounds.GetRandom(), position); // TODO: change?
 
                 ability.Cooldown = 1;
             }
+        }
+        
+        private Transform GetClosestTargetToUnit(IEnumerable<int> transformRefEntities, Vector3 unitPosition)
+        {
+            Transform bestTarget = null;
+            float closestDistanceSqr = Mathf.Infinity;
+            
+            foreach(var entity in transformRefEntities)
+            {
+                ref var transformRef = ref _transformRefPool.Value.Get(entity);
+                var directionToTarget = transformRef.Value.position - unitPosition;
+                
+                var directionToTargetSqr = directionToTarget.sqrMagnitude;
+                if (directionToTargetSqr < closestDistanceSqr)
+                {
+                    closestDistanceSqr = directionToTargetSqr;
+                    bestTarget = transformRef.Value;
+                }
+            }
+     
+            return bestTarget;
         }
     }
 }
